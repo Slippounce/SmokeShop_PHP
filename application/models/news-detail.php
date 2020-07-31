42d6bf0ea93c137c19280aa029a41f20339d7823
Slippounce <sir.shmidt-crdoc@yandex.ru>
2020-07-31 16:06:17 +0700

    getNewsById method moved to model contacts

--- application/models/news-detail.php
+++ application/models/news-detail.php
@@ -1,4 +1,15 @@
 <?php
+function getNewsById($id){
+    global $link;
+    $sql = "select * from news where id = $id";
+    if(!$result = mysqli_query($link, $sql)){
+        return false;
+    }
+    $item = mysqli_fetch_array($result);
+    //mysqli_free_result($result);
+    return $item;
+}
+
 $categories = selectAllCategories();
 $news = selectSidebarNews();
 if($categories === false || $news === false){
--- includes/lib.php
+++ includes/lib.php
@@ -86,16 +86,7 @@ function getProductById($id){
     return $item;
 }
 
-function getNewsById($id){
-    global $link;
-    $sql = "select * from news where id = $id";
-    if(!$result = mysqli_query($link, $sql)){
-        return false;
-    }
-    $item = mysqli_fetch_array($result);
-    //mysqli_free_result($result);
-    return $item;
-}
+
 
 
 
