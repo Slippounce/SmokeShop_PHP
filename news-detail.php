262eab006a88d4d490841e9d5e71e85c5cee9ca6
Slippounce <sir.shmidt-crdoc@yandex.ru>
2020-08-06 09:51:01 +0700

    changed arguments check in news-detail.php

--- news-detail.php
+++ news-detail.php
@@ -2,8 +2,8 @@
 require "includes/config.php";
 require "includes/lib.php";
 require "includes/dbconn.php";
-if(!$_GET['id'] || !is_numeric($_GET['id'])){
-        send404();
+if($_GET['id'] && (!is_numeric($_GET['id']) || (int)$_GET['id'] < 0 || !ctype_digit( $_GET['id']))){
+    send404();
 }
 require "application/models/news-detail.php";
 require "application/views/includes/template_header.php";
