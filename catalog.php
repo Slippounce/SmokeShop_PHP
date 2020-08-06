a2e5eab6b46e78f05c1e64b38f3eca9515cd828c
Slippounce <sir.shmidt-crdoc@yandex.ru>
2020-08-06 09:50:14 +0700

    changed arguments check

--- catalog.php
+++ catalog.php
@@ -4,16 +4,16 @@ require "includes/config.php";
 require "includes/lib.php";
 require "includes/dbconn.php";
 // проверить параметры
-if($_GET['id'] && !is_numeric($_GET['id'])){
+if($_GET['id'] && (!is_numeric($_GET['id']) || (int)$_GET['id'] < 0 || !ctype_digit( $_GET['id']))){
     send404();
 }
-if($_GET['price-from'] && !is_numeric($_GET['price-from'])){
+if($_GET['price-from'] && (!is_numeric($_GET['price-from']) || (int)$_GET['price-from'] < 0 || !ctype_digit( $_GET['price-from']))){
     send404();
 }
-if($_GET['price-to'] && !is_numeric($_GET['price-to'])){
+if($_GET['price-to'] && (!is_numeric($_GET['price-to']) || (int)$_GET['price-to'] < 0 || !ctype_digit( $_GET['price-to']))){
     send404();
 }
-if($_GET['page'] && !is_numeric($_GET['page'])){
+if($_GET['page'] && (!is_numeric($_GET['page']) || (int)$_GET['page'] < 0 || !ctype_digit( $_GET['page']))){
     send404();
 }
 
